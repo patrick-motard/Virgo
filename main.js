94266a765f3dfaba973c5f431ca41404d841d19c
Mark Staebler <markstaebler@gmail.com>
2015-08-07 17:10:59 +0000

    refactored to use require.js and added questions in json format

--- main.js
+++ main.js
@@ -1,41 +1,73 @@
 /*jshint curly:true, debug:true */
 
-var questionModel = function (question, answer, points){
-    var self = this;
+require(["bower_components/jquery/dist/jquery", "bower_components/knockout/dist/knockout", ], function($, ko){
     
-    self.question = question;
-    self.realAnswer = answer;
-    self.pointValue = points;
-    self.userAnswer = ko.observable("");
-    self.awarded = ko.computed(function(){
-         return self.realAnswer == self.userAnswer() ? true : false;
-    }, self);
-    
-    return self;
-}
 
 
-
-// Here's my data model
-var ViewModel = function() {
-    var self = this;
-    
-    self.firstQuestion = new questionModel('what is 1 + 1?', 2, 15);
-    
-    self.question = self.firstQuestion.question;
-    self.userAnswer = self.firstQuestion.userAnswer;
-    
-    self.time = "10";
-    self.realAnswer = self.firstQuestion.realAnswer;
-    
-    self.result = self.firstQuestion.awarded;
-    
-    
-    self.getQuestion = function(){
-        $.getJSON('http://jservice.io/api/random?count=1', function(data){
-            console.log(data);
-        })
+    var questionModel = function (question, answer, points){
+        var self = this;
+        
+        self.question = question;
+        self.realAnswer = answer;
+        self.pointValue = points;
+        self.userAnswer = ko.observable("");
+        self.awarded = ko.computed(function(){
+             return self.realAnswer == self.userAnswer() ? true : false;
+        }, self);
+        
+        return self;
     }
-};
- 
-ko.applyBindings(new ViewModel(), document.getElementById('main')); 
+    
+    var questions = [{"id":146570,
+    "answer":"lees",
+    "question":"The wine dregs",
+    "value":1000,
+    "airdate":"2014-06-23T12:00:00.000Z",
+    "created_at":"2015-01-22T02:33:28.263Z",
+    "updated_at":"2015-01-22T02:33:28.263Z",
+    "category_id":16896,
+    "game_id":4543,
+    "invalid_count":null,
+    "category":{"id":16896,"title":"l __ __ s","created_at":"2015-01-18T18:09:56.609Z","updated_at":"2015-01-18T18:09:56.609Z","clues_count":10}}];
+    
+    
+    
+    
+    
+    // Here's my data model
+    var ViewModel = function() {
+        var self = this;
+        
+        self.firstQuestion = new questionModel(questions[0].question, questions[0].answer, 15);
+        
+        self.question = self.firstQuestion.question;
+        self.userAnswer = self.firstQuestion.userAnswer;
+        
+        self.time = "10";
+        self.realAnswer = self.firstQuestion.realAnswer;
+        
+        self.result = self.firstQuestion.awarded;
+        
+        
+    //   self.getQuestion = function(){
+    //       $.ajax({
+    //           url: 'http://jservice.io/api/random?count=1',
+    //           xhrFields: {
+    //               withCredentials: true
+    //           }}).done(function( data ) {
+                
+    //               console.log( "Sample of data:", data.slice( 0, 100 ) );
+        
+    //                 });//end of .done
+    //   }
+    
+    
+            self.getQuestion = function(){
+                
+            }
+        
+    
+    }
+    ko.applyBindings(new ViewModel(), document.getElementById('main')); 
+
+});
